3681705f64424a957b3df1104f832e51feb77e94
Fabio Martins <fmadafonseca@gmail.com>
2023-01-30 10:48:31 -0300

    Exercicios de Lógica

---
+++ Logica5/Logica5/Program.cs
@@ -0,0 +1,35 @@
+﻿//Ler 10 números, somá-los e mostrar o maior número digitado
+//usando um loop while
+double N, Contagem, Maior, Soma;
+Contagem = 1;
+Soma = 0;
+Maior = 0;
+while (Contagem <= 10)
+{
+    Console.Write("Digite o " + Contagem + "°:");
+    N = Convert.ToDouble(Console.ReadLine());
+    Soma += N;
+    if (N > Maior)
+    {
+        Maior = N;
+    }
+    Contagem++;
+}
+Console.WriteLine("Soma final: " + Soma);
+Console.WriteLine("Maior número digitado: " + Maior);
+//Usando um loop for
+Console.WriteLine("--------------------------------------------------------------------------------------");
+Soma = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.Write("Digite o " + i + "°:");
+    N = Convert.ToDouble(Console.ReadLine());
+    Soma += N;
+    if (N > Maior)
+    {
+        Maior = N;
+    }
+}
+Console.WriteLine("Soma final: " + Soma);
+Console.WriteLine("Maior número digitado: " + Maior);
---
+++ Logica6/Logica6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Fazer um programa para converter reais em dólares, perguntar ao usúario a cotação e quantas conversões ele deseja fazer
+
+double Cotacao, ValorBRL, ValorUSD;
+int Conversoes;
+
+Console.WriteLine("Conversor de reais para dólares");
+Console.Write("Quantas conversões deseja fazer? ");
+Conversoes = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Qual a cotação atual do dólar? ");
+Cotacao = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 0; i < Conversoes; i++)
+{
+    Console.Write("Qual o valor em reais a ser convertido? ");
+    ValorBRL = Convert.ToDouble(Console.ReadLine());
+    ValorUSD = ValorBRL / Cotacao;
+    Console.WriteLine("O valor em dólares é:" + ValorUSD.ToString("0.00"));
+    Console.WriteLine();
+}
